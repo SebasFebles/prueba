9ec4480df6922e9675d3fa579bcf15564480de67
sebastian <sebastian_febles@hotmail.com>
2025-03-28 17:11:20 -0400

    lo mejor

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
+// Variables globales
 let habilidades = [
-    {
-        text: "Responsabilidad y puntualidad",
-        image: "imagenes/responsabilidad.jpg"
-    },
-    {
-        text: "Planificación y organización",
-        image: "imagenes/organizacion.jpg"
-    },
-    {
-        text: "Habilidad lectora y comprensiva",
-        image: "imagenes/leer.jpeg"
-    },
-    {
-        text: "Buena disposición para aprender y trabajar",
-        image: "imagenes/aprende_trabaja.jpg"
-    },
-    {
-        text: "Aprendo absolutamente rápido",
-        image: "imagenes/aprendo_rapido.jpg"
-    },
-    {
-        text: "Trabajo en equipo perfecto",
-        image: "imagenes/trabajo_equipo.jpg"
-    }
+    { text: "Responsabilidad y puntualidad", image: "imagenes/responsabilidad.jpg" },
+    { text: "Planificación y organización", image: "imagenes/organizacion.jpg" },
+    { text: "Habilidad lectora y comprensiva", image: "imagenes/leer.jpeg" },
+    { text: "Buena disposición para aprender y trabajar", image: "imagenes/aprende_trabaja.jpg" },
+    { text: "Aprendo absolutamente rápido", image: "imagenes/aprendo_rapido.jpg" },
+    { text: "Trabajo en equipo perfecto", image: "imagenes/trabajo_equipo.jpg" }
 ];
-
 let currentHabilidad = 0;
 
 // Mostrar el portafolio al hacer clic en el botón
@@ -36,10 +18,10 @@ function mostrarPortafolio() {
 // Mostrar las secciones correspondientes
 function showSection(section) {
     const sections = document.querySelectorAll('.section');
-    sections.forEach(sec => sec.classList.remove('active'));
+    sections.forEach(sec => sec.classList.remove('active')); // Elimina "active" de todas las secciones
     
     const activeSection = document.getElementById(section);
-    activeSection.classList.add('active');
+    activeSection.classList.add('active'); // Agrega "active" a la sección seleccionada
 }
 
 // Función para cambiar las habilidades
