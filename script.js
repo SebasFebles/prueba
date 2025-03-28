b99f9cbf21bdead557863aa291040f1526b7632a
sebastian <sebastian_febles@hotmail.com>
2025-03-28 02:37:40 -0400

    dasd

--- script.js
+++ script.js
@@ -27,6 +27,23 @@ let habilidades = [
 
 let currentHabilidad = 0;
 
+// Mostrar el portafolio al hacer clic en el botón
+function mostrarPortafolio() {
+    console.log("Se presionó el botón para ver el portafolio"); // Depuración
+    document.getElementById("welcome-screen").style.display = "none";  // Ocultar la pantalla de bienvenida
+    document.getElementById("portfolio").style.display = "block";      // Mostrar el portafolio
+}
+
+// Mostrar las secciones correspondientes
+function showSection(section) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.classList.remove('active'));
+    
+    const activeSection = document.getElementById(section);
+    activeSection.classList.add('active');
+}
+
+// Función para cambiar las habilidades
 function nextHabilidad() {
     currentHabilidad++;
     if (currentHabilidad >= habilidades.length) {
