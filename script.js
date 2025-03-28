a8d39325d5bb53696e4e8481973d04396e0691ee
sebastian <sebastian_febles@hotmail.com>
2025-03-28 02:29:41 -0400

    xdesx

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+// Mostrar el portafolio al hacer clic en el botón
+function mostrarPortafolio() {
+    document.getElementById("welcome-screen").style.display = "none";  // Oculta la pantalla de bienvenida
+    document.getElementById("portfolio").style.display = "block";      // Muestra el portafolio
+}
+
+// Mostrar las secciones correspondientes
+function showSection(section) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.classList.remove('active'));
+    
+    const activeSection = document.getElementById(section);
+    activeSection.classList.add('active');
+}
 let habilidades = [
     {
         text: "Responsabilidad y puntualidad",
