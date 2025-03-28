f323dc229e357ea2fbe4c72c2d4e85b850ec9460
sebastian <sebastian_febles@hotmail.com>
2025-03-27 23:09:07 -0400

    vamos haber

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+// Mostrar el portafolio al hacer clic en el botón
 function mostrarPortafolio() {
     document.getElementById("welcome-screen").style.display = "none";
     document.getElementById("portfolio").style.display = "block";
+}
+
+// Mostrar las secciones correspondientes
+function showSection(section) {
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(sec => sec.classList.remove('active'));
+    
+    const activeSection = document.getElementById(section);
+    activeSection.classList.add('active');
 }
