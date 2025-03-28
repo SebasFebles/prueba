aeea67a8fbb2db6279be065d12e6f9a5b6177a4b
sebastian <sebastian_febles@hotmail.com>
2025-03-28 02:23:26 -0400

    xd

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-// Mostrar el portafolio al hacer clic en el botón
-function mostrarPortafolio() {
-    document.getElementById("welcome-screen").style.display = "none";  // Oculta la bienvenida
-    document.getElementById("portfolio").style.display = "block";      // Muestra el portafolio
-}
+let habilidades = [
+    {
+        text: "Responsabilidad y puntualidad",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/responsabilidad.jpg"
+    },
+    {
+        text: "Planificación y organización",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/organizacion.jpg"
+    },
+    {
+        text: "Habilidad lectora y comprensiva",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/leer.jpeg"
+    },
+    {
+        text: "Buena disposición para aprender y trabajar",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/aprende_trabaja.jpg"
+    },
+    {
+        text: "Aprendo absolutamente rápido",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/aprendo_rapido.jpg"
+    },
+    {
+        text: "Trabajo en equipo perfecto",
+        image: "https://raw.githubusercontent.com/SebasFebles/prueba/main/imagenes/trabajo_equipo.jpg"
+    }
+];
 
-// Mostrar las secciones correspondientes
-function showSection(section) {
-    const sections = document.querySelectorAll('.section');
-    sections.forEach(sec => sec.classList.remove('active'));
-    
-    const activeSection = document.getElementById(section);
-    activeSection.classList.add('active');
+let currentHabilidad = 0;
+
+function nextHabilidad() {
+    currentHabilidad++;
+    if (currentHabilidad >= habilidades.length) {
+        currentHabilidad = 0;
+    }
+
+    document.getElementById("habilidadText").innerText = habilidades[currentHabilidad].text;
+    document.getElementById("habilidadImage").src = habilidades[currentHabilidad].image;
 }
